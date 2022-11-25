ff10bc71b0f35e067ba1b9b552ae7bcdc5769339
tanadej <t.chungsungnuen@itone.com>
2022-11-25 17:23:37 +0700

    add new login controller

---
+++ Controller/login.Controller.js
@@ -0,0 +1,35 @@
+const methodLogin = {
+    async login(req,res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
+
+const methodUserManage = {
+    async UserCreate(req,res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    },
+    async UserUpdate(req,res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    },
+    async UserRemove(req,res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
+
+module.exports = {...methodLogin,...methodUserManage}
